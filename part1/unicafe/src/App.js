56c8a9c4ea8e977c43a89eb48104837b12c7b229
Lucas Machado <73367620+lucsmachado@users.noreply.github.com>
2022-02-23 18:37:58 -0300

    step1

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const Heading = ({ title }) => <h1>{title}</h1>;
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  );
+}
+
+const Feedback = ({ text, number }) => <p>{text} {number}</p>;
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addOne = (state, setter) => () => setter(state + 1);
+    
+  return (
+    <div>
+      <Heading title='give feedback' />
+      <Button onClick={addOne(good, setGood)} text='good' />
+      <Button onClick={addOne(neutral, setNeutral)} text='neutral' />
+      <Button onClick={addOne(bad, setBad)} text='bad' />
+      <Heading title='statistics' />
+      <Feedback text='good' number={good} />
+      <Feedback text='neutral' number={neutral} />
+      <Feedback text='bad' number={bad} />
+    </div>
+  );
+};
+
+export default App;
