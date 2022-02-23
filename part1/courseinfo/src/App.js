3c4e4c75c2017de7844eb3151197a4babb2612cd
Lucas Machado <73367620+lucsmachado@users.noreply.github.com>
2022-02-22 21:17:19 -0300

    step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,11 +4,21 @@ const Header = (props) => {
     );
 }
 
-const Content = (props) => {
+const Part = (props) => {
     return (
         <p>
-            {props.part} {props.exercises}
+            {props.name} {props.exercises}
         </p>
+    );
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part name={props.parts[0]} exercises={props.exercises[0]} />
+            <Part name={props.parts[1]} exercises={props.exercises[1]} />
+            <Part name={props.parts[2]} exercises={props.exercises[2]} />
+        </div>
     );
 }
 
@@ -28,13 +38,14 @@ const App = () => {
     const exercises2 = 7;
     const part3 = 'State of a component';
     const exercises3 = 14;
-    
+    console.log('oi')
     return (
         <div>
             <Header course={course} />
-            <Content part={part1} exercises={exercises1} />
+            <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+            {/* <Content part={part1} exercises={exercises1} />
             <Content part={part2} exercises={exercises2} />
-            <Content part={part3} exercises={exercises3} />
+            <Content part={part3} exercises={exercises3} /> */}
             <Total exercises={exercises1 + exercises2 + exercises3} />
         </div>
     );
