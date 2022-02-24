239a0fccaa26dcd9d17955ffe384b2bcf28ac302
Lucas Machado <73367620+lucsmachado@users.noreply.github.com>
2022-02-24 14:56:50 -0300

    step7

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,11 +1,15 @@
 import Part from './Part';
+import Total from './Total';
 
 const Content = ({ parts }) => {
+  const sumExercises = (parts) => parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return (
     <>
       {parts.map((part) =>
         <Part key={part.id} part={part} />
       )}
+      <Total sum={sumExercises(parts)} />
     </>
   );
 };
---
+++ part2/courseinfo/src/components/Total.js
@@ -0,0 +1,7 @@
+const Total = ({ sum }) => {
+  return (
+    <p><strong>total of {sum} exercises</strong></p>
+  );
+};
+
+export default Total;
